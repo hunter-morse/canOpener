47d85d7aa7f0e8e17edf5c17bd3bd3f752012d48
bbons <bbons@calpoly.edu>
2021-11-27 23:18:33 -0800

    Working on tasks. Seems to have some errors with taskShare.h

---
+++ CanOpener/src/taskWriteToDisplay.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file taskWriteToDisplay.cpp
+ * @brief Header file for taskWriteToDisplay
+ * 
+ * @author Ben Bons
+ * @date Nov 27, 2021
+ * */
+#include <Arduino.h>
+#include "PrintStream.h"
+#include "shares.h"
+#include "STM32FreeRTOS.h"
+#include "taskWriteToDisplay.h"
+
+void taskWriteToDisplay(void* p_params)
+{
+    Serial.begin(115200);
+
+    // See here for why this was included (Essentially increases timing accuracy)
+    //https://canvas.calpoly.edu/courses/57860/files/5478349?wrap=1
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+
+    for (;;)
+    {
+        Serial << "Can Temperature: " << currentTemp.get() << " C" << endl;
+
+        //From the same Canvas site as above.
+        vTaskDelayUntil (&xLastWakeTime, 1000);
+    }
+}
---
+++ CanOpener/src/taskWriteToDisplay.h
@@ -0,0 +1,14 @@
+/**
+ * @file taskWriteToDisplay.h
+ * @brief Header file for taskWriteToDisplay
+ * 
+ * @author Ben Bons
+ * @date Nov 27, 2021
+ * */
+
+#ifndef TASKWRITETODISPLAY_H
+#define TASKWRITETODISPLAY_H
+
+void taskWriteToDisplay(void* p_params);
+
+#endif
