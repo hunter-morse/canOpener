fd672bb04f2262ef1dbe06f86f05ef426dea0b49
bbons <bbons@calpoly.edu>
2021-11-30 20:20:42 -0800

    Added some tasks to the main.cpp file. Also tested compiling and it compiled

--- CanOpener/src/main.cpp
+++ CanOpener/src/main.cpp
@@ -6,14 +6,42 @@
 #include <STM32FreeRTOS.h>
 #include <PrintStream.h>
 #include "shares.h"
+#include "tasks/taskLimitSwitch.h"
+#include "tasks/taskMoveMachine.h"
+#include "tasks/taskTemp.h"
+#include "tasks/taskWriteToDisplay.h"
 
 Share<float> currentTemp("Current Temperature");
-Share<bool> canDetected;
-Share<bool> canTopDetected;
-Share<bool> canTabDetected;
+Share<bool> canDetected("Can Detected");
+Share<bool> canTopDetected("Can Top");
+Share<bool> canTabDetected("Can Tab");
 
 void setup(){
-    //Setup Code
+    xTaskCreate(taskCheckLimitSwitches, 
+                "Limit Switches",
+                2048,
+                NULL,
+                4,
+                NULL);
+    xTaskCreate(taskMoveMachine,
+                "Move Machine",
+                2048,
+                NULL,
+                3,
+                NULL);
+    xTaskCreate(taskTemp,
+                "Temperatures",
+                1024,
+                NULL,
+                2,
+                NULL);
+    xTaskCreate(taskWriteToDisplay,
+                "Display",
+                2048,
+                NULL,
+                1,
+                NULL);
+    vTaskStartScheduler();
 }
 
 void loop(){
