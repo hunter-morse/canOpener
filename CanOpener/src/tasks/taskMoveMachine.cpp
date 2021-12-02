015d4c9ab22ac7d054bd10b16d2fe1cb17740ae0
bbons <bbons@calpoly.edu>
2021-12-01 16:27:03 -0800

    Changed some things on the taskMoveMachine.cpp

--- CanOpener/src/tasks/taskMoveMachine.cpp
+++ CanOpener/src/tasks/taskMoveMachine.cpp
@@ -13,19 +13,22 @@ void taskMoveMachine(void *p_params){
     MotorDriver dcMotorHead(1, DC2a, DC2b, DCEN2, pwm);     // head motor
     StepperDriver stepMotor(0, stepsPerRev, STEP1a, STEP1b, STEP1c, STEP1d, STEP1EN, stepSpeed);    // stepper motor
 
+    int16_t down_step = -4; //Amount of steps to move down each cycle
+    int16_t up_step = -1*down_step; //Amount of steps to move up each cycle
 
     // Included to increase timing accuracy (https://canvas.calpoly.edu/courses/57860/files/5478349?wrap=1)
     TickType_t xLastWakeTime = xTaskGetTickCount();
+    Serial.begin(115200);
 
     for(;;){
         // Always check canDetect
-        if(state = 0){
+        if(state = 0){ //No can in machine state
             if(canDetected.get()){
                 // if can detected then move to next state
                 Serial.println("taskLimitSwitch-state0: Can detected");     // should be written to display
                 state = 1;
             }
-        } else if(state = 1){
+        } else if(state = 1){//Moving down to find the can state
             // always make sure the can is detected
             if(!canDetected.get()){
                 // if no can detected then go back to state 0
@@ -33,40 +36,55 @@ void taskMoveMachine(void *p_params){
                 state = 0;
             } else if(canTopDetected.get()){
                 // if can top detected stop moving the head, pring message, and change state
-                stepMotor.setSpeed(0);
                 Serial.println("taskLimitSwitch-state1: Can top detected");
                 state = 2;
+                stepMotor.setPosition(0);
             } else {
                 // if can top not detected then move the head down and remain in current state
                 stepMotor.setSpeed(200);
-                stepMotor.setPosition(stepMotor.getPosition() - 50);
+                stepMotor.move(down_step);
                 // do nothing, stay in state 1
             }
         } else if (state = 2){
+            if(!canDetected.get()){
+                // if no can detected then go back to state 0
+                Serial.println("taskLimitSwitch-state1: Can not detected");
+                state = 0;
+            }
+            else
+            {
+                if(!canTopDetected.get())
+                {
+                    state = 3;
+                    Serial.println("taskLimitSwitch-state2: Moved up enough");
+                }
+                else
+                {
+                    stepMotor.move(up_step); // Move a few steps until the can top is no longer detected
+                    Serial.println("taskLimitSwitch-state2: Moving up");
+                }
+            }
+        
+        }else if (state = 3){
             // always make sure the can is detected
             if(!canDetected.get()){
                 // if no can detected then go back to state 0
-                Serial.println("taskLimitSwitch-state2: Can not detected");
+                Serial.println("taskLimitSwitch-state3: Can not detected");
                 state = 0;
             } 
             // make sure can and top are detected
-            else if (canDetected.get() && canTopDetected.get()){
+            else if (canDetected.get()){
                 // check to see if tab has been found
                 if (canTabDetected.get()){
                     // do not change state -- only change state if either the can/its top aren't detected
                     dcMotorBase.brake();        // stop rotating the base
-                    Serial.println("taskLimitSwitch-state2: Can tab found!");                   
+                    Serial.println("taskLimitSwitch-state3: Can tab found!");                   
                 } else {
-                    // tell base to rotate and remain in state 2
+                    // tell base to rotate and remain in state 3
                     dcMotorBase.move(30);       // rotate the base at 30% speed (pwm)
                 }
             } 
-            // if the can top is no longer detected
-            else if (!canTopDetected.get()){
-                // go back to the previous state
-                Serial.println("taskLimitSwitch-state2: Can top no longer detected");
-                state = 1;                
-            }
+            
             // the can is no detected
             else {
                 Serial.println("taskLimitSwitch-state2: Can no longer detected");
